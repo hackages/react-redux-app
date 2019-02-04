5d9177b93aca369e5dd2c8c9fd94691c863ca8c9
davyengone <davy@hackages.io>
2019-02-04 10:58:32 +0100

    starting point: redux-react from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+// import { connect } from 'react-redux';
 class App extends Component {
+  state = {
+    counter: 0
+  };
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 
+// const AppProvider = () => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
